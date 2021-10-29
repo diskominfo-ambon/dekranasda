bf76e54c77609f66da2e07ce5adc38a3de5f03ee
Azman Abdullah <azmanabdlh@yahoo.com>
2021-10-29 21:14:04 +0900

    menambahkan navbar

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,6 +13,23 @@
       </ul>
     </b-container>
   </div>
+  <b-navbar toggleable="lg">
+    <b-container>
+      <b-navbar-brand href="#">LOGO</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>Beranda</b-nav-item>
+          <b-nav-item>Dekranasda Maluku</b-nav-item>
+          <b-nav-item>Produk</b-nav-item>
+          <b-nav-item>Kegiatan dan Berita</b-nav-item>
+          <b-nav-item>Kontak</b-nav-item>
+          <b-nav-item>Login</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
 </header>
 
 </template>
